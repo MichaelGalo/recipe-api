867cc785fe349bd3aeed6b63ec4a6fd691ce221d
Michael Galo <michael@michaelthinks.co>
2024-10-21 15:30:21 -0500

    Removed trailing slash from CORS_ALLOWED_ORIGINS client url

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -54,7 +54,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://recipe-client-lshi3.ondigitalocean.app/",
+    "https://recipe-client-lshi3.ondigitalocean.app",
 ]
 
 MIDDLEWARE = [
