cce5efc5fd5f5fe8d31a7a5bc060fdd11cc23d2f
Michael Galo <michael@michaelthinks.co>
2024-10-21 14:52:36 -0500

    Preparing for deployment to Digital Ocean

--- recipe_api/views/auth.py
+++ recipe_api/views/auth.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from django.db import IntegrityError
 
 
 @api_view(["POST"])
